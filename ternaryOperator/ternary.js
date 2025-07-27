1f679af09de8b80d4f2f9e3868f3959bfa39fcb1
Manish Singh <manish7.2.2004@gmail.com>
2025-07-28 02:24:14 +0530

    4

---
+++ ternaryOperator/ternary.js
@@ -0,0 +1,22 @@
+// let age  = 12 ; 
+// age>18?console.log("hello"):console.log("bye") ;
+
+
+
+
+// let time  = 11 ;
+
+// let message = time>=13 ? "Good afternoon" : "good morning" ;
+
+
+// console.log(message);
+
+
+
+
+
+let purchase = 125 ;
+
+let discount = purchase > 100 ? 10 : 5 ;
+
+console.log(`your discounted price is $${purchase - purchase*(discount)/100}`);
